ac318125557bfce52afff0bad91b8f9a8327e22b
nikolapivac <62612908+nikolapivac@users.noreply.github.com>
2020-03-27 13:04:38 +0100

    Update Program.cs
    dodan komentar u main

--- PIS_vjezba1/Program.cs
+++ PIS_vjezba1/Program.cs
@@ -15,6 +15,8 @@ namespace PIS_vjezba1
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World2!");
             Console.ReadKey();
+            
+            //komentar
 
         }
     }
